42dbdfdd3083a29205b949c7ea3e26344636bd60
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2023-04-01 14:34:31 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template # """ importa classe flasc """
+
+app = Flask('__name__') #""" cria uma instnância dessa classe """
+
+@app.route('/')          #""" cria rotas com o decorator """
+def index():
+    return render_template('index.html')
+
+@app.route('/')          
+def quemsomos():
+    return render_template('/quemsomos.html')
+
+@app.route('/')          
+def contatos():
+    return render_template('/contatos.html')
+
