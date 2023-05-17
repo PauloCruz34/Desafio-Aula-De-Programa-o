6b2ceb0f29e705f3b32e2ff99a1aaf93260f2505
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2023-05-17 14:25:14 -0300

    incrementando com sql

--- app.py
+++ app.py
@@ -1,16 +1,67 @@
-from flask import Flask, render_template # """ importa classe flasc """
+from flask import Flask, render_template, request, url_for, jsonify
 
-app = Flask("__name__") #""" cria uma instnância dessa classe """
+from flask_mysqldb import MySQL
 
-@app.route("/")          #""" cria rotas com o decorator """
+
+
+app = Flask(__name__)
+
+
+
+
+# conexão com o banco de dados
+app.config['MYSQL_Host'] = 'localhost' # 127.0.0.1
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Pac109081'
+app.config['MYSQL_DB'] = 'contatos'
+
+mysql = MySQL(app)
+
+
+@app.route("/")
+@app.route("/index")
 def home():
-    return render_template ("index.html")
+    return render_template("index.html")
 
-@app.route("/quemsomos")          
-def quemsomos():
-    return render_template ("/quem_somos.html")
+@app.route("/quemsomos")
+def quem_somos():
+    return render_template("quem_somos.html")
 
-@app.route("/contatos")          
+'''
+@app.route("/contatos")
 def contatos():
-    return render_template ("/contatos.html") 
+    return render_template("contatos.html")
+
+'''
+
+
+@app.route('/contatos', methods=['GET', 'POST'])
+def contatos():
+    if request.method == "POST":
+        email = request.form['email']
+        assunto = request.form['assunto']
+        descricao = request.form['descricao']
+        
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO contatos(email, assunto, descricao) VALUES (%s, %s, %s)", (email, assunto, descricao))
+       
+        mysql.connection.commit()
+        
+        cur.close()
+
+        return 'sucesso'
+    return render_template('contatos.html')
+
+
+# rota usuários para listar todos os usuário no banco de dados.
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+
+    users = cur.execute("SELECT * FROM contatos")
+
+    if users > 0:
+        userDetails = cur.fetchall()
+
+        return render_template("users.html", userDetails=userDetails)
 
