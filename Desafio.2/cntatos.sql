291d05e1161b52df75af559bcea93c5e0b329186
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2023-06-06 14:32:58 -0300

    aws

---
+++ Desafio.2/cntatos.sql
@@ -0,0 +1,13 @@
+create database contatos;
+
+use contatos;
+
+create table contatos(
+	email varchar(30),
+    assunto varchar(60),
+    descricao varchar(200)
+    
+    );
+
+select * from contatos  #seleciona todos os dados na tabela contatos
+
